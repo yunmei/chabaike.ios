b59353884ebe02646af37d45b7f64402fd05e77f
bevin1984 <bevin1984@gmail.com>
2013-03-19 11:05:35 +0800

    增加ListViewController

---
+++ chabaike/ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  chabaike
+//
+//  Created by bevin chen on 13-3-19.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UIViewController
+
+@end
---
+++ chabaike/ListViewController.m
@@ -0,0 +1,38 @@
+//
+//  ListViewController.m
+//  chabaike
+//
+//  Created by bevin chen on 13-3-19.
+//
+//
+
+#import "ListViewController.h"
+
+@interface ListViewController ()
+
+@end
+
+@implementation ListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
