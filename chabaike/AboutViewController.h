ab20e57c2ae9853e3ed4a0bbd9216e3992ef2e76
bevin1984 <bevin1984@gmail.com>
2013-03-20 09:19:40 +0800

    添加AboutViewController

---
+++ chabaike/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  chabaike
+//
+//  Created by bevin chen on 13-3-20.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
